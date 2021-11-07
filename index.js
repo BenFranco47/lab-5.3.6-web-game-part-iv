cc0f6eb001a6df394e4cd50d918690658e38a974
Ben Franco <88692753+BenFranco47@users.noreply.github.com>
2021-11-06 23:22:45 -0400

    my first commit

--- index.js
+++ index.js
@@ -1,9 +1,84 @@
 const inventory = newInventory()
 move(inventory).to(0, 0)
 
-const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
 
+// This code can be modify so other objects, characters or items can be moved by the user
+const character = newImage('assets/green-character/static.gif')
+
+// The (e) KeyboardEvent fires multiple times as long as the user holds down a key
+function handleDirectionChange(direction) {
+//Loads multiple gifs so my character can seem like it's moving 
+    if(direction === null) {
+        character.src = 'assets/green-character/static.gif'
+    }
+    if(direction === 'west') {
+        character.src = 'assets/green-character/west.gif'
+    }
+    if(direction === 'north') {
+        character.src = 'assets/green-character/north.gif'
+    }
+    if(direction === 'east') {
+        character.src = 'assets/green-character/east.gif'
+    }
+    if(direction === 'south') {
+        character.src = 'assets/green-character/south.gif'
+    }
+}
+
+
+move(character).withArrowKeys(100 ,250 ,handleDirectionChange)
+
+
+setInterval(function(){ 
+    if(direction === 'west'){
+        x = x - 1
+    }
+    if(direction === 'north'){
+        y = y + 1
+    }
+    if(direction === 'east'){
+        x = x + 1
+    }
+    if(direction === 'south'){
+        y = y - 1
+    }
+    character.style.left = x + 'px'
+    character.style.bottom = y + 'px'
+}, 1)
+
+// Keydown dom event moves the character in the given direction (up, down, right and left)
+document.addEventListener('keydown', function(e) {
+   
+    if(e.repeat) return;
+
+
+    if(e.key === 'ArrowLeft') {
+        direction = 'west'
+    }
+
+    if(e.key === 'ArrowUp') {
+        direction = 'north'
+    }
+
+    if(e.key === 'ArrowRight') {
+        direction = 'east'
+    }
+
+    if(e.key === 'ArrowDown') {
+        direction = 'south'
+    }
+})
+
+//Keyup dom event stops the character after you press the arrow keys
+// Both dom events work together to give the user move control over the movement of the character
+document.addEventListener('keyup', function(e){
+    direction = null
+})
+
+
+// Call moveCharacter every millisecond
+//setInterval(moveCharacter, 1)
+move(character).to(100, 250)
 
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
